f7a790e34c531115efd3587c9b3bf806b99b12fc
naosgmt1 <naosgmt1@gmail.com>
2020-01-31 17:56:52 +0900

    new file:   mmpmgr2/Procfile 	new file:   mmpmgr2/__pycache__/reference.cpython-37.pyc 	new file:   mmpmgr2/app.py 	new file:   mmpmgr2/manifest.yml 	new file:   mmpmgr2/reference.py 	new file:   mmpmgr2/requirements.txt 	new file:   mmpmgr2/runtime.txt 	new file:   mmpmgr2/static/ECSv2.png 	new file:   mmpmgr2/static/PiedPiper.png 	new file:   mmpmgr2/static/backgr2.jpg 	new file:   mmpmgr2/static/logo.png 	new file:   mmpmgr2/static/pcf.png 	new file:   mmpmgr2/static/style.css 	new file:   mmpmgr2/templates/aboutus.html 	new file:   mmpmgr2/templates/mainmenu.html 	new file:   mmpmgr2/templates/upload_music.html 	new file:   mmpmgr2/uploads/donotremove.txt

---
+++ mmpmgr2/app.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+### IMPORT ###
+import uuid
+import time
+import datetime
+import os
+import sys
+# import re
+import boto
+import redis
+import json
+import requests
+import reference as ref
+#from flask import Flask, render_template, redirect, request, Response, make_response, url_for
+from flask import Flask, render_template, redirect, request, Response, make_response
+#from werkzeug import secure_filename
+#from werkzeug import utils
+import werkzeug
+
+### PARAMETER ###
+dtnow = datetime.datetime.now()
+print (dtnow)
+
+    #### ECS version ####
+# bname = "mplaylist"
+# ecs_access_key_id = "131745189033837095@ecstestdrive.emc.com"
+# ecs_secret_key = "RS/P25qnWmi5C8uGYaBojzAzYXf8dnrNHOYbao9d"
+# ecs_host = "object.ecstestdrive.com"
+# access_url = "131745189033837095.public.ecstestdrive.com"
+
+session = boto.connect_s3(aws_access_key_id=ref.ecs_access_key_id, \
+                          aws_secret_access_key=ref.ecs_secret_key, \
+                          host=ref.ecs_host)
+b = session.get_bucket(ref.bname)
+
+if ('VCAP_SERVICES' in os.environ):
+    VCAP = json.loads(os.environ['VCAP_SERVICES'])
+    CREDENTIALS = VCAP["rediscloud"][0]["credentials"]
+    r = redis.Redis(host=CREDENTIALS["hostname"], port=CREDENTIALS["port"], password=CREDENTIALS["password"])
+    print ("Now running in PCF")
+    print ("VCAP is ",end=": ")
+    print (VCAP)
+else:
+    print ("ERROR in VCAP_SERVICES", end=": ")
+    print (VCAP)
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = set(['mp3', 'MP3', 'mp4', 'MP4'])
+
+def allowed_file(filename):
+    return ('.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS'])
+
+### Read stations list
+def rstations():
+#   global access_url
+#   global bname
+
+   readurl = str ("https://" + ref.access_url + "/" + ref.bname + "/" + "stationslist.txt")
+   st = requests.get(readurl)
+   if (st.status_code == 200):
+      print ("SUCCESSFUL to GET CMD\n")
+   else:
+      print ("read error@rcmd() as code =" + str(st.status_code))
+   f = [st.status_code, st.text]
+   return f
+
+### Read cmdlist
+def rcmd():
+   readurl = str("https://" + ref.access_url + "/" + ref.bname + "/" + "cmdlist.txt")
+   print (readurl)                # should delete
+   r = requests.get(readurl)
+   if (r.status_code == 200):
+      print ("SUCCESSFUL to GET CMD\n")
+   else:
+      print ("read error@rcmd() as code =" + str(r.status_code))
+   return [r.status_code, r.text]
+
+### MAIN PAGE ###
+@app.route('/')
+def menu():
+    global r
+    r.incr('hitcounter')
+    musiccount = r.get('mcounter')
+    reviewcount = r.get ('rcounter')
+    usercount = r.get('ucounter')
+    uuid = request.cookies.get('uuid')
+    if not uuid:
+        print ("uuid cookie was not present")
+        uuid = r.incr('ucounter')
+
+    resp = "Hello Music!"
+
+    print ("resp is ", end=": ")
+    print (resp)
+
+    resp = make_response (render_template('mainmenu.html', musiccnt=musiccount, ucount=usercount))
+
+    print ("resp is ", end=": ")
+    print (resp)
+
+    resp.set_cookie('uuid', str(uuid), max_age=604800)
+
+    print ("RETURNING MAIN PAGE")
+
+    return resp
+#     return render_template('main_menu.html')
+
+@app.route('/upload_music.html')
+def index():
+    global r
+    r.incr('hitcounter')
+    print ("Now, upload_music.html")
+    return render_template('upload_music.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    global size
+    global b
+    global r
+
+    rfile = request.files['file']
+    print (rfile.filename)
+    if rfile and allowed_file(rfile.filename):
+#        print (datetime.now().strftime("%Y%m%d_%H%M%S_"))
+        # Make the filename safe, remove unsupported chars
+#        savename = datetime.now().strftime("%Y%m%d_%H%M%S_") \
+#                 + werkzeug.utils.secure_filename(rfile.filename)
+        savename = werkzeug.utils.secure_filename(rfile.filename)
+        print (savename)
+        print ("am here")
+        justname = savename.rsplit(".",1)[0]
+        justname = justname + str (int (time.time() * 1000))
+        print (justname)
+        rfile.save(os.path.join(app.config['UPLOAD_FOLDER'], savename))
+
+        print ("Uploading " + savename + " as key " + justname)
+        k = b.new_key(savename)
+        k.set_contents_from_filename(app.config['UPLOAD_FOLDER'] + savename)
+        k.set_acl('public-read')
+
+        os.remove(app.config['UPLOAD_FOLDER'] + savename)
+
+        uuid = request.cookies.get('uuid')
+        Counter = r.incr('pcounter')
+        print ("the music counter is now: ")
+        print (Counter)
+        newmusic = 'music' + str(Counter)
+        print ("Lets create Redis hash: ")
+        print (newmusic)
+#        r.hmset(newmusic,{'name':justname, 'uuid':uuid})
+        r.hmset(newmusic,{'name':savename, 'uuid':uuid})
+        return"""
+        <html>
+          <head>
+            <link rel=stylesheet type=text/css href="/static/style.css">
+            <meta name="viewport" content="width=410, initial-scale=0.90">
+          </head>
+          <body>
+            <div class="container">
+            <div class="content">
+            <h3>Thank You!</h3>
+            <h4>You have just uploaded a Music to: <br><a href="https://portal.ecstestdrive.com/">ECS Object Storage</a></h4>
+            <a href="/"><h3>Back to main menu</h3></a>
+        <img src="/static/logo.png" width="270" />
+        """
+    else: 
+        return "ERROR in UPLOADING FILE WAS FOUND"
+
+@app.route('/showmusics')
+def musics():
+    global r
+    global bname
+    r.incr('hitcounter')
+    musiccount = r.get("pcounter")
+    musicfiles = "<div><label><input type=\"checkbox\" name=\"NO SELECT\" value=\"\"/></label></div>"
+
+    for each_music in r.keys('music*'):
+        print (each_music)
+        decfilen = str(r.hget(each_music,'name'), encoding='utf-8')
+        print (decfilen)
+        musicfiles = musicfiles + "<div><label><input type=\"checkbox\" name=\"musicfiles\" value=\"" \
+                   + decfilen \
+                   + "\"/>" \
+                   + decfilen \
+                   + "</label></div>"
+    print ("musicfiles", end=": ")
+    print (musicfiles)
+    f = rstations()
+    print("===Kicked rstatinos=== with",end=":")
+    print(f)
+    slist = json.loads (f[1])
+#    print ("type of slist", end=": ")
+#    print (type (slist['stations']['station']))
+    musicstations = ""
+    for each_station in slist['stations']:
+#    for each_station in slist:
+        musicstations = musicstations + "<option value=" \
+                      + each_station['link'] \
+                      + ">" \
+                      + each_station['station'] \
+                      + "</option>"
+
+#    print("slist",end=": ")
+#    print(slist)
+    print("musicstatinos",end=": ")
+    print(musicstations)
+    pagepart1 = """
+	<html>
+	  <head>
+	    <link rel=stylesheet type=text/css href=\"static/style.css\">
+	    <meta name=\"viewport\" content="width=410, initial-scale=0.90">
+
+	  </head>
+	  <body>
+	     <div class=\"container\">
+	     <div class=\"content\">
+	     <img src=\"static/logo.png\" width=\"270\" />
+	     <h2>Total musics uploaded: {{ musiccount }} </h2>
+	     <a href=\"/\"><h3>Back to main menu</h3></a>
+	     <form action=\"setplaylist\" method=\"post\">
+	        <body style=\"background-color: grey;\">
+    """.format(musiccount)
+
+    pagepart2 = """
+	        </body>
+	        <br><br>
+	        <h2>STATIONS LIST</h2>
+	        <h4>picked up from shoutcast.com</h4>
+	        <body style=\"backgroune-color: grey;\">
+	            <select name=\"selectedstation\" size=\"4\">
+    """
+
+    pagepart3 = """
+	            </select>
+	            <br><br>
+		    <input class="btn-blue" type="submit" value="SET PLAYLIST">
+		    <br>
+	        </body>
+	     </form>
+	  </body>
+	</html>
+    """
+
+    filetable = pagepart1 + musicfiles + pagepart2 + musicstations + pagepart3
+    print (filetable)
+    return filetable
+
+@app.route('/setplaylist', methods=['POST'])
+def setplaylist():
+    global b
+    mf = request.form.getlist("musicfiles")       # musicfiles
+    ss = request.form.get("selectedstation")      # selectedstation
+    text = ""
+
+    print("mf", end=" = ")
+    print(mf)
+    print("ss", end=" = ")
+    print(ss)
+
+    for each_file in mf:
+       print ("each file", end=": ")
+       print (each_file)
+       text = text + each_file + "\n"
+
+    text = text + ss + "\n"
+
+    print("playlist is",end=" : ")
+    print(text)
+    print(type(text))
+
+    key = b.new_key ('playlist.txt')
+    key.set_contents_from_string(text)
+    key.set_acl('public-read')
+
+    return "Set playlist.txt, now"
+
+@app.route('/playbutton')
+def playbutton():
+    global dtnow
+    global b
+
+    f = rcmd()
+    cmdlist = json.loads (f[1])
+    print (cmdlist)              # should delete
+    print (cmdlist['kicked date']) # should delete
+    print (cmdlist['operation']) # should delete
+
+    if (cmdlist['kicked date'] == "1980/01/01 00:00:01"):
+       resp = "PREVIOUS COMMAND HAS NOT BEEN FINISHED"
+    else:
+       cmdlist['kicked date'] = "1980/01/01 00:00:01"
+       cmdlist['entry date'] = "{0:%Y/%m/%d %H:%M:%S}".format(dtnow)
+       if (cmdlist['operation'] == "stop"):
+           cmdlist['operation'] = "play"
+           cmdop = "play"
+       else: 
+           cmdlist['operation'] = "stop"
+           cmdop = "stop"
+
+       key = b.new_key ('cmdlist.txt')
+       key.set_contents_from_string(json.dumps(cmdlist))
+       key.set_acl('public-read')
+       resp = "SUCCESS to SET OPERATION" + cmdop
+    print ("playbutton", end=": ")
+    print (resp)
+    return resp
+
+@app.route('/aboutus')
+def aboutus():
+    global r
+    r.incr('hitcounter')
+    resp = make_response(render_template('aboutus.html'))
+    print (resp)
+    return resp
+
+
+if __name__ == "__main__":
+	app.run(debug=False, host='0.0.0.0', \
+                port=int(os.getenv('PORT', '5000')), threaded=True)
+
+
---
+++ mmpmgr2/reference.py
@@ -0,0 +1,5 @@
+bname = "mplaylist"
+ecs_access_key_id = "131745189033837095@ecstestdrive.emc.com"
+ecs_secret_key = "RS/P25qnWmi5C8uGYaBojzAzYXf8dnrNHOYbao9d"
+ecs_host = "object.ecstestdrive.com"
+access_url = "131745189033837095.public.ecstestdrive.com"

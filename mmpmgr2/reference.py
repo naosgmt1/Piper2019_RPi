01e657cc9b35a9c6e7f0e5364ff0714f6ab43061
naosgmt1 <naosgmt1@gmail.com>
2020-02-06 12:11:08 +0900

    modified:   reference.py

--- mmpmgr2/reference.py
+++ mmpmgr2/reference.py
@@ -1,5 +1,5 @@
 bname = "mplaylist"
-ecs_access_key_id = "131745189033837095@ecstestdrive.emc.com"
-ecs_secret_key = "RS/P25qnWmi5C8uGYaBojzAzYXf8dnrNHOYbao9d"
+ecs_access_key_id = "xxxxxxxxxxxxxxxxxx@ecstestdrive.emc.com"
+ecs_secret_key = "pppppaaaaasssssswwwwwwooooorrrrrrddddddd"
 ecs_host = "object.ecstestdrive.com"
-access_url = "131745189033837095.public.ecstestdrive.com"
+access_url = "xxxxxxxxxxxxxxxxxx.public.ecstestdrive.com"
